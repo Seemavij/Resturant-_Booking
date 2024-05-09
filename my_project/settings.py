1c472a59ac959bd261bb97cfe74d337b52e38a83
Seema Vij <seemavij@live.ie>
2024-05-09 20:13:49 +0000

    update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,9 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 
-
-import os
 from pathlib import Path
+import os
 import dj_database_url
 if os.path.isfile("env.py"):
     import env
@@ -87,12 +86,12 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+ #  'default': {
+    #   'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))

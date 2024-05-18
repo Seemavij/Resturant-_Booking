bded8e9c6f81d13145a9494893437488d05bc009
Seema Vij <seemavij@live.ie>
2024-05-18 20:20:01 +0000

    deployment setting

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -14,8 +13,9 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+           ) from exc
+    execute_from_command_line
+    (sys.argv)
 
 
 if __name__ == '__main__':

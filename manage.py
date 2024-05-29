173bf0d8f0cfaefa30d791b95bf4f31d6c948973
Seema Vij <seemavij@live.ie>
2024-05-29 21:46:42 +0000

    upload image

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
@@ -13,8 +14,10 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-         ) from exc
-    execute_from_command_line(sys.argv)
+              ) from exc
+    execute_from_command_line (sys.argv)
+
 
 if __name__ == '__main__':
-     main()
+         main()
+         

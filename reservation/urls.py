664126c6f479d9a5b231c5ebf333bf316ba31f72
Seema Vij <seemavij@live.ie>
2024-05-29 08:09:06 +0000

    add some code in base.html

--- reservation/urls.py
+++ reservation/urls.py
@@ -7,6 +7,8 @@ app_name = 'reservation'
 urlpatterns = [
 
      path('', views.table_reservation, name='table_reservation'),
+     path('manage_reservation/',
+         views.BookingsList.as_view(), name='manage_reservation'),
      path('edit_reservation/<reservation_id>/', views.edit_reservation,
          name='edit_reservation'),
      path('delete_reservation/<reservation_id>/', views.delete_reservation,
